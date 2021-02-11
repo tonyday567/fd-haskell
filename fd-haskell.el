4d1177508439854704c98ce3ffcdee0d438f1ed3
Chris Perivolaropoulos <cperivol@Chriss-MacBook-Air.local>
2021-02-11 19:32:50 +0000

    Package header

--- fd-haskell.el
+++ fd-haskell.el
@@ -1,3 +1,33 @@
+;;; fd-haskell.el --- summary -*- lexical-binding: t -*-
+
+;; Author: Chris Perivolaropoulos
+;; Maintainer: Chris Perivolaropoulos
+;; Version: 0.0.2
+;; Keywords: haskell
+
+
+;; This file is not part of GNU Emacs
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; Haskell
+
+;;; Code:
+
 (require 'compile)
 (require 'flycheck)
 
