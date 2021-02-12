0de55cb9f5875962df627dd860441677e5c13871
Chris Perivolaropoulos <cperivol@Chriss-MacBook-Air.local>
2021-02-12 20:28:55 +0000

    expanded file names

--- fd-haskell-tags.el
+++ fd-haskell-tags.el
@@ -1,11 +1,12 @@
 (require 'haskell-cabal)
 
 (defun haskell-cabal--find-tags-dir-ad (fn &rest r)
-  (or
-   (locate-dominating-file default-directory "tags")
-   (locate-dominating-file default-directory "TAGS")
-   (locate-dominating-file default-directory "stack.yaml")
-   (apply fn r)))
+  (flet ((get-dir (x) (expand-file-name (locate-dominating-file default-directory x))))
+    (or
+     (get-dir "tags")
+     (get-dir "TAGS")
+     (get-dir "stack.yaml")
+     (apply fn r))))
 
 (advice-add 'haskell-cabal--find-tags-dir :around #'haskell-cabal--find-tags-dir-ad)
 (provide 'fd-haskell-tags)
